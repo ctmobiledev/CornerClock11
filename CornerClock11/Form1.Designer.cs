12b24af341d0e1f7fda9944cb50c5ebdf18dde00
CTMobileDev <smallscreensw@hotmail.com>
2021-11-17 19:51:51 -0600

    Initial code push

---
+++ CornerClock11/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CornerClock11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(-5, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(248, 68);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(232, 86);
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ CornerClock11/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * 
+ *      CORNER CLOCK 11, a Windows 11 system tray clock alternative
+ * 
+ *      Version 1.0.0
+ *      
+ *      Charles Tatum II - charlestatumuiuc@outlook.com
+ * 
+ *      Runs a static digital clock in 24-hour mode in the lower right corner of
+ *      a Windows 11 screen. Replacement for the removal of the digital clock when
+ *      the time/date was clicked in the system tray.
+ * 
+ */ 
+
+namespace CornerClock11
+{
+    public partial class Form1 : Form
+    {
+
+        private Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.BackColor = this.lblTime.BackColor;
+            this.TransparencyKey = this.lblTime.BackColor;
+            lblTime.Visible = false;
+
+            // Seed with current time
+            String timeNow = DateTime.Now.ToString("HH:mm:ss");
+            timeNow = lblTime.Text;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Text = "CornerClock11";
+            notifyIcon1.Visible = true;
+
+            // Reposition to lower right corner above system tray
+            this.Top = SystemInformation.VirtualScreen.Height - this.Height - 72;
+            this.Left = SystemInformation.VirtualScreen.Width - this.Width;
+
+            // Set up to continue running until killed off; 1/10 second will help smooth time changes
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Interval = 100;                           
+            timer.Start();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            timer = null;
+            this.Dispose();
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            timer = null;
+            this.Dispose();
+            Application.Exit();
+        }
+
+        public void TimerTick(Object myObject, EventArgs myEventArgs)
+        {
+            String timeNow = DateTime.Now.ToString("HH:mm:ss");
+            lblTime.Text = timeNow;
+            lblTime.Visible = true;
+        }
+    }
+}
